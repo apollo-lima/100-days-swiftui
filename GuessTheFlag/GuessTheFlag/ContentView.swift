f979975392d7cae0428749522422fd77d8ebe9ee
apollolima <apollolima@icloud.com>
2024-04-30 12:52:50 +0100

    Project 2, Guess The Flag App [ Stacks, Colors and Frames, Gradients, Buttons and Images and Alerts]

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -5,52 +5,106 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
+    
+    @State private var questions = 0
+    @State private var gameOver = false
+    
     
     var body: some View {
         ZStack {
             Color.yellow
-            .ignoresSafeArea()
+                .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundStyle(.indigo)
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.indigo)
-                }
-                
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
+            VStack {
+                Spacer()
+                Text("Guess The Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(.indigo)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.buttonBorder)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 10)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            if scoreTitle == "Wrong" {
+                Text("Wrong! That' the flag of \(countries[correctAnswer])")
+            } else {
+                Text("That's right! Well done!")
+            }
+        }
+        .alert("Game Over", isPresented: $gameOver) {
+            Button("Restart", action: restart)
+        } message: {
+            Text("Game Over. Your Final Score is \(score)")
         }
     }
     
     func flagTapped(_ number: Int) {
+        questions += 1
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
             scoreTitle = "Wrong"
+            
+            if score <= 0 {
+                score = 0
+            } else {
+                score -= 1
+            }
         }
-        
         showingScore = true
+        
+        if questions == 8 {
+            gameOver = true
+        }
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    func restart() {
+        score = 0
+        askQuestion()
+    }
 }
 
-//#Preview {
-//    ContentView()
-//}
+#Preview {
+    ContentView()
+}
