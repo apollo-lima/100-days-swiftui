0b7a82e8a61f25eca4ed751e8f4bc66ae335d8b7
apollolima <apollolima@icloud.com>
2024-04-30 08:51:52 +0100

    Project 2 - continue from Styling our flags

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by SAD Lima on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
