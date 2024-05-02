ab9b53fe186929be72224f727290afaea2ea7aa1
apollolima <apollolima@icloud.com>
2024-05-02 11:53:15 +0100

    Porject 4, BetterRest - Skeleton

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by SAD Lima on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
