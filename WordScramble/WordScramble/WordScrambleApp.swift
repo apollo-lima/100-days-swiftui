4b986de0c30152b0a44cf135723a9551fdc3104e
apollolima <apollolima@icloud.com>
2024-05-02 13:47:33 +0100

    Project 5, WordScramble App

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by SAD Lima on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
