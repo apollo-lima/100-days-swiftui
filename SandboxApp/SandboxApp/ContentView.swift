d2bbaa2fda3d3176cc8760777d5bce36737917d8
apollolima <apollolima@icloud.com>
2024-05-08 07:55:18 +0100

    Project 7, iExpense - Partial

---
+++ SandboxApp/SandboxApp/ContentView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add number") {
+                    numbers.insert(currentNumber, at: 0)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SandboxApp/SandboxApp/SandboxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxAppApp.swift
+//  SandboxApp
+//
+//  Created by Apollo Lima on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SandboxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
